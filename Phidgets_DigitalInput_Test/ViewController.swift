b8ae9f4a0099acb576732af5dda11938997a8f0f
joelisreal <joel.igberase@gmail.com>
2019-04-17 13:07:26 -0600

    Added pod file, imported phidget module and wrote code for button press.

--- Phidgets_DigitalInput_Test/ViewController.swift
+++ Phidgets_DigitalInput_Test/ViewController.swift
@@ -7,14 +7,52 @@
 //
 
 import UIKit
+import Phidget22Swift
 
 class ViewController: UIViewController {
 
+    let button = DigitalInput()
+    
+    func attach_handler(sender: Phidget){
+        print("Button Attached")
+    }
+    
+    func state_change(sender: DigitalInput, state: Bool){
+        if(state == true){
+            print("Button Pressed")
+        }
+        else{
+            print("Button Not Pressed")
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        do {
+            //enable server discovery
+            try Net.enableServerDiscovery(serverType: .deviceRemote)
+            
+            //address object
+            try button.setDeviceSerialNumber(528025)
+            try button.setHubPort(0)
+            try button.setIsHubPortDevice(true)
+            
+            //add attach handler
+            let _ = button.attach.addHandler(attach_handler)
+            
+            //add state change handler
+            let _ = button.stateChange.addHandler(state_change)
+            
+            //open object
+            try button.open()
+            
+        } catch let err as PhidgetError {
+            print("Phidget Error " + err.description)
+        } catch {
+            //catch other errors here
+        }
     }
-
-
 }
 
